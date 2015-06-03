45df2061428a59135e219e4158276b7be470aaeb
schreiby <ettie.schreiber@gmail.com>
2015-06-03 19:22:30 -0400

    Initial commit

---
+++ app/src/main/java/com/example/student/notificationdemo/NotificationReceiverActivity.java
@@ -0,0 +1,15 @@
+package com.example.student.notificationdemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by student on 6/3/2015.
+ */
+public class NotificationReceiverActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.notification);
+    }
+}
